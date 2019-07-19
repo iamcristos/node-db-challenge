9fb3042ebd21baf5dda47529a0b1ec506f3f3349
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-07-19 09:28:29 +0100

    -created migration schemas

--- data/migrations/20190719091119_createProjectActionTables.js
+++ data/migrations/20190719091119_createProjectActionTables.js
@@ -1,8 +1,34 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+            .createTable('projects', tbl =>{
+                tbl.increments()
+                tbl.string('project_name')
+                    .unique()
+                    .notNullable()
+                tbl.text('project_description')
+                    .notNullable()
+                tbl.boolean('project_completed')
+                    .notNullable()
+                    .defaultTo(false)
+            })
+            .createTable('actions', tbl => {
+                tbl.increments()
+                tbl.text('action_description')
+                    .unique()
+                    .notNullable()
+                tbl.integer('project_id')
+                    .unsigned()
+                    .references('projects.id')
+                tbl.text('action_notes')
+                tbl.boolean('action_completed')
+                    .notNullable()
+                    .defaultTo(false)
+            })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+             .dropTableIfExists('projects')
+             .dropTableIfExists('actions')
 };
